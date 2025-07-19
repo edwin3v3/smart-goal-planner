0db430379a298e2e274132f13d1808d3cfe06a7d
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-07-20 01:41:53 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <div className='align-top'>
+      <h1>Smart Goal Planner</h1>
+      
+    </div>
+      
+    </>
+  )
+}
+
+export default App
